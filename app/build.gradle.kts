625c34804d7a93512afe8c5c184e2f57ac661730
绯歌 <94056084+FeiGePro@users.noreply.github.com>
2026-07-27 13:49:25 +0800

    release: bump version to 0.2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "io.github.feigepro.checkintrace"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "0.2.3"
+        versionCode = 15
+        versionName = "0.2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
