587f9772d0653896e51481f7545cf1cbc212b7b2
FeiGePro <1774607619@qq.com>
2026-08-20 11:35:51 +0800

    release: prepare CheckinTrace v0.2.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "io.github.feigepro.checkintrace"
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
-        versionName = "0.2.4"
+        versionCode = 16
+        versionName = "0.2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
